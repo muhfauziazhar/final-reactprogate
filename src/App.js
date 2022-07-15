16a5b93d9caf8cf3f083abcc33624a6c02b1ae8d
muhfauziazhar <muhammad.fauzi73@ui.ac.id>
2022-07-15 14:39:50 +0700

    all crud done

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import TodoForm from "./components/TodoForm";
+import TodosList from "./components/TodosList";
+import "./App.css";
+
+const App = () => {
+    const initialState = JSON.parse(localStorage.getItem("todos")) || [];
+    const [input, setInput] = useState("");
+    const [todos, setTodos] = useState(initialState);
+    const [editTodo, setEditTodo] = useState(null);
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+    return (
+        <div className="container">
+            <div className="app-wrapper">
+                <div className="header">
+                    <h1>My Todos List</h1>
+                </div>
+                <div>
+                    <TodoForm
+                        input={input}
+                        setInput={setInput}
+                        todos={todos}
+                        setTodos={setTodos}
+                        editTodo={editTodo}
+                        setEditTodo={setEditTodo}
+                    />
+                </div>
+                <div>
+                    <TodosList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
